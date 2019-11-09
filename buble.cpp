e6d0b5be16d4ae5d8f4c8545f1c068c9fc66cba6
karaziiatania <karaziia.tania@gmail.com>
2019-11-09 23:43:31 +0200

    add buble sort

--- buble.cpp
+++ buble.cpp
@@ -1,3 +1,27 @@
-void buble(){
+#include <iostream>
+#include<locale>
+#include<cstdlib>
 
+using namespace  std;
+
+void buble(){
+    setlocale(LC_ALL,"RUS");
+    int ar[1000],i,k,rasmer,d;
+    cout<<"размер";
+    cin>> rasmer;
+    for (i=0;i<rasmer; i++) {
+        ar[i]=rand()%1000;
+        cout<<ar[i]<<" ";
+    }
+    for (i=0;i<rasmer; i++) {
+        for (k=1;k<rasmer-i; k++){
+            if(ar[k-1]>ar[k]){
+                d=ar[k];
+                ar[k]=ar[k-1];
+                ar[k-1]=d;
+            }
+        }
+    }
+    cout<< "масив:";
+    for(i=0; i<rasmer; i++)cout<<ar[i]<<" ";
 }
